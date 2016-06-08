9939f77d9a90ee65cb22fb75fe7884869acd5db8
Kevin <kevin.mayo@udacity.com>
2016-06-07 19:08:54 -0700

    Add find_by_title

--- lib/product.rb
+++ lib/product.rb
@@ -8,11 +8,16 @@ class Product
   def self.all
     @@products
   end
+  def self.find_by_title(title)
+    @@products.find do |product|
+      title == product.title
+    end
+  end
   def add_to_products
     if !@@products.any?{|product| product.title == @title}
       @@products << self
     else
-      raise DuplicateProductError, "'#{@title}' already exists."
+      #raise DuplicateProductError, "'#{@title}' already exists."
     end
   end
 end
