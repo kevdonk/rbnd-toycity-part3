85b72cb7dce49ef8f13866810358efc81be088bc
Kevin <kevin.mayo@udacity.com>
2016-06-07 19:23:34 -0700

    Add customer class

---
+++ lib/customer.rb
@@ -0,0 +1,23 @@
+class Customer
+  attr_reader :name, :price, :stock
+  @@customers = []
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+  def self.all
+    @@customers
+  end
+  def self.find_by_name(name)
+    @@customers.find do |customer|
+      name == customer.name
+    end
+  end
+  def add_to_customers
+    if !@@customers.any?{|customer| customer.name == @name}
+      @@customers << self
+    else
+      #raise DuplicateCustomerError, "'#{@name}' already exists."
+    end
+  end
+end
