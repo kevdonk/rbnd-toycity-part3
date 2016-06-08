ad9a7411d5d27574df1e58df873b0c316b69c601
Kevin <kevin.mayo@udacity.com>
2016-06-07 19:19:03 -0700

    Add basic product properties

--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,10 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
   @@products = []
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
   def self.all
@@ -13,11 +15,19 @@ class Product
       title == product.title
     end
   end
+  def self.in_stock
+    @@products.select do |product|
+      product.in_stock?
+    end
+  end
   def add_to_products
     if !@@products.any?{|product| product.title == @title}
       @@products << self
     else
       #raise DuplicateProductError, "'#{@title}' already exists."
     end
+  end
+  def in_stock?
+    @stock > 0
   end
 end
