5a6c7c575eb893885c38151152b5d151a9aaddf1
Kevin <kevin.mayo@udacity.com>
2016-06-07 18:47:25 -0700

    Add add_to_products or error

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,18 @@
 class Product
+  attr_reader :title
   @@products = []
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
   def self.all
     @@products
+  end
+  def add_to_products
+    if !@@products.any?{|product| product.title == @title}
+      @@products << self
+    else
+      raise DuplicateProductError, "'#{@title}' already exists."
+    end
   end
 end
